2ec154e288d2d688f724f1f86513f385c7de782b
Andrii-Cherevatov <alwayswannafly2121@gmail.com>
2023-10-07 01:21:39 +0300

    Add middleware to a chain

--- password-task/password-task/Program.cs
+++ password-task/password-task/Program.cs
@@ -1,4 +1,5 @@
 using password_task.Interfaces;
+using password_task.Middlewares;
 using password_task.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,6 +27,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.MapControllers();
 
